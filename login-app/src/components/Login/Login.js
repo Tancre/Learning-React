da09914ffca4d8eaad4904ff23fa4be5a1ab4243
Tancre <outisisout@gmail.com>
2023-08-05 12:30:14 +0200

    Debouncing to wait user stops typing

--- login-app/src/components/Login/Login.js
+++ login-app/src/components/Login/Login.js
@@ -12,9 +12,12 @@ const Login = (props) => {
   const [formIsValid, setFormIsValid] = useState(false);
 
   useEffect(() => {
-    setFormIsValid(
-      enteredEmail.includes('@') && enteredPassword.trim().length > 6
-    );
+    setTimeout(() => {
+      setFormIsValid(
+        enteredEmail.includes('@') && enteredPassword.trim().length > 6
+      );
+    }, 500);
+
   }, [enteredEmail, enteredPassword]);
 
   const emailChangeHandler = (event) => {
