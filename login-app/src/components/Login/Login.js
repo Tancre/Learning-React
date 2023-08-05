bba8a5c5cee0dc1b0fb3a9f21211c76293b9f6f2
Tancre <outisisout@gmail.com>
2023-08-05 13:41:03 +0200

    fix inverted logic email password

--- login-app/src/components/Login/Login.js
+++ login-app/src/components/Login/Login.js
@@ -6,20 +6,20 @@ import Button from "../UI/Button/Button";
 
 const emailReducer = (state, action) => {
   if (action.type === 'USER_INPUT') {
-    return {value: action.val, isValid: action.val.trim().length > 6}
+    return {value: action.val, isValid: action.val.includes('@')}
   }
   if (action.type === 'INPUT_BLUR') {
-    return {value: state.value, isValid: state.value.trim().length > 6}
+    return {value: state.value, isValid: state.value.includes('@')}
   }
   return { value: "",  isValid: false,};
 };
 
 const passwordReducer = (state, action) => {
   if (action.type === 'USER_INPUT') {
-    return {value: action.val, isValid: action.val.includes('@')}
+    return {value: action.val, isValid: action.val.trim().length > 6}
   }
   if (action.type === 'INPUT_BLUR') {
-    return {value: state.value, isValid: state.value.includes('@')}
+    return {value: state.value, isValid: state.value.trim().length > 6}
   }
   return { value: "",  isValid: false,};
 }
