bb2a416beafc7d4a91a34e9af3342e9badbea7de
Tancre <outisisout@gmail.com>
2023-08-06 20:18:18 +0200

    removed useless (props)

--- login-app/src/components/MainHeader/Navigation.js
+++ login-app/src/components/MainHeader/Navigation.js
@@ -4,7 +4,7 @@ import AuthContext from '../../store/auth-context';
 import classes from './Navigation.module.css';
 
 
-const Navigation = (props) => {
+const Navigation = () => {
   const ctx = useContext(AuthContext);
 
   return (
