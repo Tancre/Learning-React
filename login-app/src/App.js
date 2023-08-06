fcf31b77e780ca3aafe36e747ad1765f8091ed61
Tancre <outisisout@gmail.com>
2023-08-06 20:09:34 +0200

    logout with context

--- login-app/src/App.js
+++ login-app/src/App.js
@@ -29,8 +29,13 @@ function App() {
   };
 
   return (
-    <AuthContext.Provider value={{ isLoggedIn: isLoggedIn }}>
-      <MainHeader onLogout={logoutHandler} />
+    <AuthContext.Provider
+      value={{
+        isLoggedIn: isLoggedIn,
+        isLoggedOut: logoutHandler,
+      }}
+    >
+      <MainHeader />
       <main>
         {!isLoggedIn && <Login onLogin={loginHandler} />}
         {isLoggedIn && <Home onLogout={logoutHandler} />}
--- login-app/src/components/MainHeader/Navigation.js
+++ login-app/src/components/MainHeader/Navigation.js
@@ -22,7 +22,7 @@ const Navigation = (props) => {
         )}
         {ctx.isLoggedIn && (
           <li>
-            <button onClick={props.onLogout}>Logout</button>
+            <button onClick={ctx.onLogout}>Logout</button>
           </li>
         )}
       </ul>
