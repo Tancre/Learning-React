9fd320db773ab627d3ba6efc7410d8b518a1c9cb
Tancre <outisisout@gmail.com>
2023-08-07 10:46:41 +0200

    added meals seciton

--- food-order-app/src/App.js
+++ food-order-app/src/App.js
@@ -1,9 +1,13 @@
 import Header from "./Layout/Header/Header";
+import Meals from "./Meals/Meals";
 
 function App() {
   return (
     <>
       <Header />
+      <main>
+        <Meals />
+      </main>
     </>
   );
 }
