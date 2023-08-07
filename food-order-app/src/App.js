e2c21419e63a6a29a2a7e05b7f604d93abcd0172
Tancre <outisisout@gmail.com>
2023-08-07 10:32:11 +0200

    added header button

--- food-order-app/src/App.js
+++ food-order-app/src/App.js
@@ -2,9 +2,9 @@ import Header from "./Layout/Header/Header";
 
 function App() {
   return (
-    <div>
+    <>
       <Header />
-    </div>
+    </>
   );
 }
 
--- food-order-app/src/Layout/Header/Header.js
+++ food-order-app/src/Layout/Header/Header.js
@@ -3,12 +3,14 @@ import React from 'react'
 import mealsImage from '../../Assets/meals.jpg'
 import classes from './Header.module.css'
 
+import HeaderCartButton from '../../UI/HeaderCartButton'
+
 const Header = props => {
   return (
     <>
       <header className={classes.header}>
         <h1>ReactMeals</h1>
-        <button>Cart</button>
+        <HeaderCartButton />
       </header>
       <div className={classes['main-image']}>
         <img src={mealsImage} alt="A table full of delicious food" />
